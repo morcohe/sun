077b12409ff533d94f59557265ea43d5ee83efab
Mor <morcoh@cloudzone.io>
2023-08-20 18:01:28 +0300

    fix invisible buttons

--- components/GeneralPage.tsx
+++ components/GeneralPage.tsx
@@ -75,31 +75,31 @@ const GeneralPage = (props: any) => {
         let colMap: any = {};
         let counter = 0;
         let emptyColumns = [];
-        
+
         for await (const col of columns) {
             colMap[col.column] = false;
         }
-        
+
         for await (const row of rows) {
             for await (const key of Object.keys(row)) {
-                if(Object.keys(colMap).includes(key)){
-                    if(row[key].length > 0 && colMap[key] === false){
+                if (Object.keys(colMap).includes(key)) {
+                    if (row[key].length > 0 && colMap[key] === false) {
                         colMap[key] = true;
                         counter++;
-                        if(Object.keys(colMap).length <= counter){
+                        if (Object.keys(colMap).length <= counter) {
                             break;
                         }
                     }
-                    if(Object.keys(colMap).length <= counter){
+                    if (Object.keys(colMap).length <= counter) {
                         break;
                     }
                 }
-                
+
             }
         }
-        if(Object.keys(colMap).length > counter){
+        if (Object.keys(colMap).length > counter) {
             for await (const key of Object.keys(colMap)) {
-                if(colMap[key] === false){
+                if (colMap[key] === false) {
                     emptyColumns.push(key);
                 }
             }
@@ -111,23 +111,26 @@ const GeneralPage = (props: any) => {
 
 
     const initColumns = async (columns: Array<any>, rows: any) => {
-        const emptyColumns = await getEmptyColumns(columns, rows);
-        let tmpCols = [];
         for await (const col of columns) {
-            let colName = '';
-            if(Object.keys(col).includes("column")){
-                colName=col.column;
-            } else {
-                colName = col.name;
-            }
             fields.push({
-                label: colName[0].toUpperCase() + colName.slice(1),
-                key: colName,
+                label: col.column[0].toUpperCase() + col.column.slice(1),
+                key: col.column,
                 fieldType: {
                     type: 'input'
                 }
             })
-            if(emptyColumns.includes(colName)){
+        }
+        const emptyColumns = await getEmptyColumns(columns, rows);
+        let tmpCols = [];
+        for await (const col of columns) {
+            let colName = '';
+            if (Object.keys(col).includes("column")) {
+                colName = col.column;
+            } else {
+                colName = col.name;
+            }
+
+            if (emptyColumns.includes(colName)) {
                 continue;
             } else {
                 let tmp: any = {
@@ -145,16 +148,16 @@ const GeneralPage = (props: any) => {
                 tmpCols.push(tmp);
             }
         }
-        
+
         let filteredRows = [];
-        for await(const row of rows){
+        for await (const row of rows) {
             let count = 0;
-            for await(const i of tmpCols){
-                if(row[i["dataIndex"]]?.length > 0){
+            for await (const i of tmpCols) {
+                if (row[i["dataIndex"]]?.length > 0) {
                     filteredRows.push(row);
                     break;
                 }
-                if(count >= tmpCols?.length){
+                if (count >= tmpCols?.length) {
                     break;
                 }
                 count++;
@@ -167,11 +170,11 @@ const GeneralPage = (props: any) => {
             width: '50px',
             fixed: 'right'
         });
-        
-        
+
+
         cols = [...tmpCols];
 
-        
+
         await init(`/api/page?what=columns,records&who=${props?.page}`, tmpCols, filteredRows);
     }
 
@@ -200,11 +203,9 @@ const GeneralPage = (props: any) => {
 
 
 
-        {
-            cols.length > 0 && <div style={{ height: "70vh", marginTop: "-20px", overflow: "scroll", position: "absolute", width: "100%", marginLeft: "0%", paddingLeft: "4%", paddingRight: "5%" }}>
-                <Editable refreshTable={refreshTable} tmpCurEditingSelect={tmpCurEditingSelect} user={user} accessConfig={pageAccessConfig} addRow={addRow} setTmpCurEditingTags={setTmpCurEditingTags} setTmpCurEditingSelect={setTmpCurEditingSelect} editingKey={editingKey} deleteRow={deleteRow} edit={edit} cancel={cancel} save={save} width={cols?.length > 0 ? cols?.length*150 : 1000} height="49vh" title={""} page={props.page} data={data} columns={cols} form={form} isEditing={isEditing} fetchData={fetchData} handlers={{ filterHandler }} fields={fields} importURL={`/api/record?createType=multi&page=${props?.page}`} apiURL={`/api/page?what=columns,records&who=${props?.page}`} import={false} export={false} createPath={`/dashboard/management/pages/table/${props.page}/column/create`} createTitle="Add Column" originColumns={columns} />
-            </div>
-        }
+        <div style={{ height: "70vh", marginTop: "-20px", overflow: "scroll", position: "absolute", width: "100%", marginLeft: "0%", paddingLeft: "4%", paddingRight: "5%" }}>
+            <Editable refreshTable={refreshTable} tmpCurEditingSelect={tmpCurEditingSelect} user={user} accessConfig={pageAccessConfig} addRow={addRow} setTmpCurEditingTags={setTmpCurEditingTags} setTmpCurEditingSelect={setTmpCurEditingSelect} editingKey={editingKey} deleteRow={deleteRow} edit={edit} cancel={cancel} save={save} width={cols?.length > 0 ? cols?.length * 150 : 1000} height="49vh" title={""} page={props.page} data={data} columns={cols} form={form} isEditing={isEditing} fetchData={fetchData} handlers={{ filterHandler }} fields={fields} importURL={`/api/record?createType=multi&page=${props?.page}`} apiURL={`/api/page?what=columns,records&who=${props?.page}`} import={false} export={false} createPath={`/dashboard/management/pages/table/${props.page}/column/create`} createTitle="Add Column" originColumns={columns} />
+        </div>
 
 
     </div >
